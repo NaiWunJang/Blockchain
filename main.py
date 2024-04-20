59190123b0dcf8ca0e7f3591ae8bab4ad3f28111
NaiWunJang <91878423+NaiWunJang@users.noreply.github.com>
2024-04-20 14:12:22 +0000

    main

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
+
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+  return render_template('about.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+
+# if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=8080)
